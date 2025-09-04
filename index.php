00469289426d8a4c6508f77b132e7e91b225bde3
bhushan <bhushanpathakbackup@gmail.com>
2025-09-04 09:03:22 +0000

    Add sample PHP file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+<?php
+// Sample PHP code for index.php
+
+// Display a welcome message
+echo "<h1>Welcome to the App Service Example!</h1>";
+
+// Show current date and time
+echo "<p>Current server time: " . date("Y-m-d H:i:s") . "</p>";
+
+// Simple array and loop example
+$colors = ["Red", "Green", "Blue"];
+echo "<ul>";
+foreach ($colors as $color) {
+    echo "<li>$color</li>";
+}
+echo "</ul>";
